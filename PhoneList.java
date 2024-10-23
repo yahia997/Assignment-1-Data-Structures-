0270761fb686802e3b1128374339936401c3b9e8
Yahya Zakaria <78583513+yahia997@users.noreply.github.com>
2024-10-23 22:37:16 +0300

    Add files via upload

---
+++ PhoneList.java
@@ -0,0 +1,31 @@
+public class PhoneList {
+  ContactNode head; // reference to the head
+
+  // Inner class
+  class ContactNode {
+    // attributes
+    String name, phonNumber;
+    ContactNode next; // reference to the next node
+
+  }
+  public static void main(String args[]) {
+
+  }
+  // Methods
+  public static void addContact(String name, String phoneNumber) {
+
+  }
+
+
+  public static void displayList() {
+
+  }
+
+  public static void searchContact(String name) {
+
+  }
+
+  public static void deleteContact(String name) {
+
+  }
+}
