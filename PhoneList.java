5613a8edd321a088009ef24ef423fb62ef045316
Yahya Zakaria <78583513+yahia997@users.noreply.github.com>
2024-10-23 22:42:13 +0300

    Update PhoneList.java

--- PhoneList.java
+++ PhoneList.java
@@ -7,6 +7,17 @@ public class PhoneList {
     String name, phonNumber;
     ContactNode next; // reference to the next node
 
+    // First constructor without next node
+    ContactNode(String name, String phonNumber) {
+      this.name = name;
+      this.phonNumber = phonNumber;
+    }
+
+    // Second constructor with next node
+    ContactNode(String name, String phonNumber, ContactNode next) {
+      this(name, phonNumber); // calls First constuctor
+      this.next = next;
+    }
   }
   public static void main(String args[]) {
 
